1dc5051b99273b830a5d7131b69725aeca4bebc8
zay <zaymaungmaungmyint@advent-soft.com>
2023-04-10 09:52:08 +0630

    update evn file for confiruration

--- server/database/db.config.js
+++ server/database/db.config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-    HOST: process.env.DB_HOST || 'localhost',
-    USER: process.env.DB_USER || 'root',
-    PASSWORD: process.env.DB_PASSWORD || 'root123',
-    DB: process.env.DB_NAME || "eighti_access_control",
+    HOST: process.env.MYSQL_DB_HOST || 'localhost',
+    USER: process.env.MYSQL_DB_USERNAME || 'root',
+    PASSWORD: process.env.MYSQL_DB_PASSWORD || 'root123',
+    DB: process.env.MYSQL_DB_URL || "eighti_access_control",
     dialect: process.env.DB_DIALECT || "mysql",
     pool: {
       max: 5,
